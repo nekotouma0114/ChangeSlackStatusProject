016105a0c4ecc09a570d3f619c3d63a0923f23f0
nekotouma0114 <nekotouma0114@gmail.com>
2020-04-25 00:45:16 +0900

    [Add] sam buildでbuildする方法が見つからなかったので代替のビルド用シェルを追加。合わせてreadmeにbuild->deployの流れを追記

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#NOTE: I dont know how to build by "sam build"
+cargo build --release --target x86_64-unknown-linux-musl
+zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap
