ce0aa3a7f92f2e0f6617358d172889cf74ad20f3
nekotouma0114 <nekotouma0114@gmail.com>
2020-04-25 00:23:34 +0900

    First commit(sample code)

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use simple_error::bail;
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Deserialize, Clone)]
+struct CustomEvent {
+    first_name: String,
+    last_name: String,
+}
+
+#[derive(Serialize, Clone)]
+struct CustomOutput {
+    message: String,
+}
+
+fn main() {
+    lambda!(my_handler);
+}
+
+fn my_handler(e: CustomEvent, ctx: Context) -> Result<CustomOutput, HandlerError> {
+    if e.first_name == "" {
+        bail!("Empty first name");
+    }
+    Ok(CustomOutput{
+        message: format!("Hello, {}!", e.first_name),
+    })
+}
