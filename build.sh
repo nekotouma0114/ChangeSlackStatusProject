210bd9c1e1867c4a3f02a13e345a4e9f99bdfd40
nekotouma0114 <nekotouma0114@gmail.com>
2020-05-03 15:56:39 +0900

    [Clean] #1 認証情報ファイルがgoogle(GCP)のもので分かるように変更

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 #NOTE: I dont know how to build by "sam build"
 cargo build --release --target x86_64-unknown-linux-musl
-zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap secret.json
+zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap google_secret.json
